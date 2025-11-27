40d65cc0cb7438080e6e49d08b38fe550109e759
Ceydanur Dinç <235060006@toros.edu.tr>
2025-11-27 18:10:36 +0300

    Add files via upload

---
+++ Media.cs
@@ -0,0 +1,52 @@
+﻿namespace SWE201_235060006
+{
+    public class Media
+    {
+        private string title;
+        private string type;        // Film or Series
+        private int releaseYear;
+
+        // Properties
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public int ReleaseYear
+        {
+            get => releaseYear;
+            set => releaseYear = value;
+        }
+
+        // Empty constructor
+        public Media() { }
+
+        // Parameterized constructor
+        public Media(string title, string type, int releaseYear)
+        {
+            this.title = title;
+            this.type = type;
+            this.releaseYear = releaseYear;
+        }
+
+        // Member function
+        public string GetDetails()
+        {
+            return $"{Title} ({ReleaseYear}) - Tür: {Type}";
+        }
+
+        // ToString override
+        public override string ToString()
+        {
+            return $"[Media] {Title} - {Type}, {ReleaseYear}";
+        }
+    }
+}
+
---
+++ MediaProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SWE201_235060006
+{
+    class MediaProgram
+    {
+        static void Main(string[] args)
+        {
+            // Empty constructor
+            Media m1 = new Media();
+            m1.Title = "Breaking Bad";
+            m1.Type = "Dizi";
+            m1.ReleaseYear = 2008;
+
+            // Parameterized constructor
+            Media m2 = new Media("Inception", "Film", 2010);
+            Media m3 = new Media("Dark", "Dizi", 2017);
+            Media m4 = new Media("Interstellar", "Film", 2014);
+
+            Console.WriteLine(m1.GetDetails());
+            Console.WriteLine(m2.GetDetails());
+            Console.WriteLine(m3.GetDetails());
+            Console.WriteLine(m4.GetDetails());
+
+            // ToString example
+            Console.WriteLine(m4.ToString());
+        }
+    }
+}
+
